46a82a544fd2e1c2b7c147b52d7ca0e10c62e46f
Javier <javier.rostagno@gmail.com>
2025-06-27 11:44:08 -0300

    feat:filter componets

--- src/presentations/components/transactions/TransactionListItem.tsx
+++ src/presentations/components/transactions/TransactionListItem.tsx
@@ -9,11 +9,11 @@ interface TransactionListItemProps {
 const TransactionListItem = (props: TransactionListItemProps) => {
   const { transactionType, amount, date } = props;
   return (
-    <li className="flex items-center justify-between py-3 border-b px-2">
-      <div className="flex gap-1 items-center">
+    <li className="flex items-center justify-between px-2 py-3 border-b">
+      <div className="flex items-center gap-1">
         <SvgTransactionIcon />
         <div className="">
-          <h3 className="text-sm text-textDark leading-4 font-semibold">
+          <h3 className="text-sm font-semibold leading-4 text-textDark">
             Link de pago
           </h3>
           <p className="text-sm font-thin leading-4 text-textNeutral">
@@ -22,7 +22,7 @@ const TransactionListItem = (props: TransactionListItemProps) => {
         </div>
       </div>
       <div className="flex flex-col">
-        <strong className="font-semibold text-success text-sm">{`$${formatPrice(
+        <strong className="text-sm font-semibold text-success">{`${formatPrice(
           amount,
           2
         )}`}</strong>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,24 @@
-// // tailwind.config.js
-// module.exports = {
-//   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// };
-
-// tailwind.config.js
 import defaultTheme from "tailwindcss/defaultTheme";
 
 export default {
+  darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // asegurate que apunta bien
   theme: {
     extend: {
       fontFamily: {
-        sans: ['"Public Sans"', ...defaultTheme.fontFamily.sans],
+        sans: ['Public Sans"', ...defaultTheme.fontFamily.sans],
       },
       colors: {
         primaryBrand: "#022A9A",
-        secondary: "#172B4D",
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
         textPrimary: "#3A3A3A",
-        accent: "#36B37E",
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
         textDark: "#313643",
         textNeutral: "#606882",
         textGray: "#565656",
@@ -34,10 +31,45 @@ export default {
         blue: "#3564FD",
         error: "#EF4444",
         success: "#1C8367",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        chart: {
+          1: "hsl(var(--chart-1))",
+          2: "hsl(var(--chart-2))",
+          3: "hsl(var(--chart-3))",
+          4: "hsl(var(--chart-4))",
+          5: "hsl(var(--chart-5))",
+        },
       },
       borderRadius: {
         xl: "1.5rem",
         layout: "32px",
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
       spacing: {
         safe: "env(safe-area-inset-top)",
@@ -45,5 +77,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
