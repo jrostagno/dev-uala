fed23e36ba692f83e12e6a0e3c487bde5a90af44
Javier <javier.rostagno@gmail.com>
2025-06-30 22:06:44 -0300

    feat:env

--- src/actions/getTransactions.ts
+++ src/actions/getTransactions.ts
@@ -3,8 +3,11 @@ import type { Transaction } from "@/infrastructure/interfaces/paymentsApi";
 const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export const fetchTransactions = async (): Promise<Transaction[]> => {
-  await sleep(2000);
-  const res = await fetch("/api/transactions");
+  await sleep(1000);
+
+  const url = import.meta.env.VITE_API_URL;
+
+  const res = await fetch(url);
 
   if (!res.ok) {
     throw new Error("Error al obtener transacciones");
